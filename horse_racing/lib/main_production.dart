4e69cad88477770d9d665f1df269c16649229213
someone7140 <ti7140@mail.goo.ne.jp>
2022-03-20 15:09:52 +0900

    パス設定の修正

--- horse_racing/lib/main_production.dart
+++ horse_racing/lib/main_production.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:horse_racing/widget/horse_racing_widget.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env.production");
+  setUrlStrategy(PathUrlStrategy());
   runApp(const HorceRacingApp());
 }
